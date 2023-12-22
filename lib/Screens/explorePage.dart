c1b884e4500ec6a1d54e02382ff80d97fef13b14
Sherli7 <51827917+Sherli7@users.noreply.github.com>
2023-12-22 14:56:58 +0100

    ReviewForm:Our Goals : Create the review form widget and Inputs for text and rating

--- lib/Screens/explorePage.dart
+++ lib/Screens/explorePage.dart
@@ -4,17 +4,36 @@ class ExplorePage extends StatefulWidget {
 
   const ExplorePage({super.key});
 
-
   @override
-  State<ExplorePage> createState() => _MyExplorePageState();
+  MyExplorePageState createState()=>MyExplorePageState();
 }
 
-class _MyExplorePageState extends State<ExplorePage> {
+class MyExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Explore Page'
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: Column(
+        children:<Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderSide:BorderSide(
+                  color:Colors.grey,
+                  width: 2.0
+                ),
+              ),
+              contentPadding: EdgeInsets.all(5.0),
+            ),
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+            ),
+          ),
+          //GridView.builder(gridDelegate: null, itemBuilder: null)
+        ],
       ),
     );
   }
